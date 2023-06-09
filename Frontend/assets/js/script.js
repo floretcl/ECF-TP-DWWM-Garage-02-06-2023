bfe16b89f0c5575675891a12ce67b9db7b77106d
floretcl <floretcl@outlook.com>
2023-06-09 17:29:54 +0200

    mobile version integration of home page

---
+++ Frontend/assets/js/script.js
@@ -0,0 +1,50 @@
+const buttonMenuMobile = document.getElementById('btn-menu-mobile');
+const menuMobile = document.getElementById('menu-mobile');
+const menuMobileLine1 = document.getElementById('btn-menu-mobile-line1');
+const menuMobileLine2 = document.getElementById('btn-menu-mobile-line2');
+const menuMobileLine3 = document.getElementById('btn-menu-mobile-line3');
+
+const toggleMenuMobile = () => {
+    menuMobile.classList.toggle('menu-mobile-active');
+}
+
+const toggleButtonMenuMobile = () => {
+    menuMobileLine2.classList.toggle('opacity-0');
+    menuMobileLine1.classList.toggle('rotate-45');
+    menuMobileLine3.classList.toggle('-rotate-45');
+    menuMobileLine1.classList.toggle('translate-y-2.5');
+    menuMobileLine3.classList.toggle('-translate-y-2');
+}
+
+buttonMenuMobile.addEventListener('click', () => {
+    toggleMenuMobile();
+    toggleButtonMenuMobile();
+});
+
+//----------------------------------------------------------//
+
+const buttons = document.getElementsByClassName('btn-gradient-animation');
+
+const btnHoverAnimation = (button) => {
+    button.classList.remove('btn__bg--hover-out');
+    button.classList.add('btn__bg--hover');
+}
+
+const btnHoverAnimationReverse = (button) => {
+    button.classList.remove('btn__bg--hover');
+    button.classList.add('btn__bg--hover-out');
+}
+
+for (const button of buttons) {
+    button.addEventListener('mouseenter', () => {
+        btnHoverAnimation(button);
+    });
+}
+
+for (const button of buttons) {
+    button.addEventListener('mouseleave', () => {
+        btnHoverAnimationReverse(button);
+    });
+}
+
+//----------------------------------------------------------//
