c3d5b29339f9db65f8622806b46e05459333e47b
Floretcl <38957472+floretcl@users.noreply.github.com>
2023-07-02 18:17:08 +0200

    modify var let to const

--- Frontend/assets/js/leaflet.js
+++ Frontend/assets/js/leaflet.js
@@ -6,7 +6,7 @@ const zoomLevel = 16;
 const maxZoom = 19;
 const minZoom = 10;
 
-let leafletMap = L.map('leaflet-map').setView([lon, lat], zoomLevel);
+const leafletMap = L.map('leaflet-map').setView([lon, lat], zoomLevel);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   minZoom: minZoom,
@@ -15,7 +15,7 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(leafletMap);
 
-let garageVParrotPlaceMarker = L.marker([lon, lat], {
+const garageVParrotPlaceMarker = L.marker([lon, lat], {
   title: 'Garage V. Parrot',
   alt: 'Emplacement du garage V. Parrot',
 })
