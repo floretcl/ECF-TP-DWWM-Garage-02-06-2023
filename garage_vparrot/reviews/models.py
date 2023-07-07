1ac5ffcefd7b736d43aae664a6326076d74400b0
Floretcl <38957472+floretcl@users.noreply.github.com>
2023-07-07 23:02:54 +0200

    add CustomerReview model class

--- garage_vparrot/reviews/models.py
+++ garage_vparrot/reviews/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class CustomerReview(models.Model):
+    name = models.CharField("nom", max_length=20)
+    message = models.TextField("message")
+    rating = models.SmallIntegerField("note")
+    verified = models.BooleanField("vérifié")
+    valid = models.BooleanField("valide", null=True)
+    date = models.DateTimeField("date")
