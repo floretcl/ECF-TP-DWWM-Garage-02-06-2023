ad78ad1ef740490b9b4a0a81d94be1abf20463e2
Floretcl <38957472+floretcl@users.noreply.github.com>
2023-07-08 19:52:56 +0200

    remove an useless import fix home page : remove "/" from path

--- garage_vparrot/garage_vparrot/urls.py
+++ garage_vparrot/garage_vparrot/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from garage import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('garage.urls')),
+    path('', include('garage.urls')),
 ]
