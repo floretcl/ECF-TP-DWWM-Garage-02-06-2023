4f3760eaf91528326fcf1858f38ebeba13a152e2
Floretcl <38957472+floretcl@users.noreply.github.com>
2023-07-10 16:59:01 +0200

    add settings for debug toolbar

--- garage_vparrot/garage_vparrot/settings.py
+++ garage_vparrot/garage_vparrot/settings.py
@@ -127,3 +127,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#
+#
+# Custom
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
--- garage_vparrot/garage_vparrot/urls.py
+++ garage_vparrot/garage_vparrot/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+import debug_toolbar
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
     path('', include('garage.urls')),
+    path('__debug__/', include(debug_toolbar.urls)),
 ]
