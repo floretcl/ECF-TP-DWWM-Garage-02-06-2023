136f12caf1e8ae6cd19d9b018594f00919554cf6
Floretcl <38957472+floretcl@users.noreply.github.com>
2023-07-10 16:58:43 +0200

    add views as Class (generic view from django)

--- garage_vparrot/garage/urls.py
+++ garage_vparrot/garage/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = "garage"
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('vehicles/', views.VehiclesView.as_view(), name='vehicles'),
+    path('contact/', views.ContactView.as_view(), name='contact'),
+    path('legal-notice/', views.LegalNoticeView.as_view(), name='legal-notice'),
+    path('privacy-policy/', views.PrivacyPolicyView.as_view(), name='privacy-policy'),
 ]
--- garage_vparrot/garage/views.py
+++ garage_vparrot/garage/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from django.views import generic
 
-def index(request):
-    return HttpResponse("Hello world")
+class IndexView(generic.TemplateView):
+    template_name = 'garage/index.html'
+
+class VehiclesView(generic.TemplateView):
+    template_name = 'garage/vehicles.html'
+
+class ContactView(generic.TemplateView):
+    template_name = 'garage/contact.html'
+
+class LegalNoticeView(generic.TemplateView):
+    template_name = 'garage/legal-notice.html'
+
+class PrivacyPolicyView(generic.TemplateView):
+    template_name = 'garage/privacy-policy.html'
