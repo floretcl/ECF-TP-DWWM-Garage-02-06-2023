3bcd56af910e2352aab5ab1a9f3a7a9191d05368
Floretcl <38957472+floretcl@users.noreply.github.com>
2023-07-14 18:17:02 +0200

    update garage/views.py : contactView implement generic FormView

--- garage_vparrot/garage/views.py
+++ garage_vparrot/garage/views.py
@@ -1,16 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
 from django.views import generic
+from .modelforms import ContactForm
 
-class IndexView(generic.TemplateView):
+class IndexView(generic.View):
     template_name = 'garage/index.html'
 
 class VehiclesView(generic.TemplateView):
     template_name = 'garage/vehicles.html'
 
-class ContactView(generic.TemplateView):
+class ContactView(generic.FormView):
     template_name = 'garage/contact.html'
+    form_class = ContactForm
+    success_url = '/contact/'
+    
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
 
 class LegalNoticeView(generic.TemplateView):
     template_name = 'garage/legal-notice.html'
