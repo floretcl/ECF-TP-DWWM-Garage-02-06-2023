0d4e25c16b9d5605795e5ee42ea74a858ba758d5
Floretcl <38957472+floretcl@users.noreply.github.com>
2023-07-14 18:56:35 +0200

    add a custom filter tag that can add css classes to label_tag

--- garage_vparrot/garage/templatetags/garage_extras.py
+++ garage_vparrot/garage/templatetags/garage_extras.py
@@ -2,6 +2,10 @@ from django import template
 
 register = template.Library()
 
-@register.filter(name='addclass')
-def addclass(field, classes):
+@register.filter(name='add_class')
+def add_class(field, classes):
     return field.as_widget(attrs={"class":classes})
+
+@register.filter(is_safe=True)
+def label_tag_with_classes(field, classes):
+    return field.label_tag(attrs={"class":classes})
