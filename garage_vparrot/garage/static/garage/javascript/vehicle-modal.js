5fc941b09d48b0d9f2019cea07ab75ecffcfe0c8
Floretcl <38957472+floretcl@users.noreply.github.com>
2023-07-19 12:38:29 +0200

    adapt vehicle page with js and json data

--- garage_vparrot/garage/static/garage/javascript/vehicle-modal.js
+++ garage_vparrot/garage/static/garage/javascript/vehicle-modal.js
@@ -1,15 +1,9 @@
-//--------------------- MODAL : VEHCILES CONTACT FORM  ---------------------------//
+//--------------------- MODAL : VEHICLES CONTACT FORM  ---------------------------//
 
 const modalVehicles = document.getElementById('modal-vehicles');
-const modalVehiclesOpenBtns = document.getElementsByClassName('modal-vehicles-open-btn');
 const modalVehiclesCloseBtn = document.getElementById('modal-vehicles-close-btn');
-
-for (const button of modalVehiclesOpenBtns) {
-  button.addEventListener('click', () => {
-    modalVehicles.classList.remove('hidden');
-  });
-}
 
 modalVehiclesCloseBtn.addEventListener('click', () => {
   modalVehicles.classList.add('hidden');
+  modalFormInputSubject.value = "";
 });
