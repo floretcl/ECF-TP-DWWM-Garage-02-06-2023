c5fb026f3ae66ade2731e2150aa8d90d85367298
Floretcl <38957472+floretcl@users.noreply.github.com>
2023-10-03 19:09:49 +0200

    update, add validators, fix and clean models.py

--- garage_vparrot/garage/models.py
+++ garage_vparrot/garage/models.py
@@ -2,152 +2,186 @@ from django.db import models
 from django.utils import timezone
 from django.core import validators
 
+
 class Garage(models.Model):
-    name = models.CharField("nom", max_length=50, primary_key=True)
-    adress = models.CharField("adresse", max_length=255)
-    postal_code = models.CharField("code postal", max_length=20)
+    name = models.CharField("nom", max_length=50)
+    address = models.CharField("adresse", max_length=255)
+    postal_code = models.CharField("code postal", max_length=10)
     city = models.CharField("ville", max_length=80)
-    phone_number = models.CharField("numéro de téléphone", max_length=30)
-    contact_email = models.CharField("email de contact", max_length=80)
+    phone_number = models.CharField(
+        "numéro de téléphone",
+        max_length=20,
+        validators=[
+            validators.RegexValidator(
+                regex=r'^0[1-9](\d{8})$',
+                message="Entrez un numéro de téléphone français valide, ex: 0123456789.")
+        ]
+    )
+    contact_email = models.CharField(
+        "email de contact",
+        max_length=254,
+        validators=[
+            validators.EmailValidator(message="L'adresse email est invalide.")
+        ]
+    )
 
     def __str__(self) -> str:
         return self.name
-    
+
     class Meta:
-        verbose_name = "Garage"
+        verbose_name = "garage"
+
 
 class OpeningTime(models.Model):
     class Day(models.TextChoices):
-        MONDAY = 'Lun.', "Lundi"
-        TUESDAY = 'Mar.', "Mardi"
-        WEDNESDAY = 'Mer.', "Mercredi"
-        THURSDAY = 'Jeu.', "Jeudi"
-        FRIDAY = 'Ven.', "Vendredi"
-        SATURDAY = 'Sam.', "Samedi"
-        SUNDAY = 'Dim.', "Dimanche"
+        MONDAY = "Lun.", "Lundi"
+        TUESDAY = "Mar.", "Mardi"
+        WEDNESDAY = "Mer.", "Mercredi"
+        THURSDAY = "Jeu.", "Jeudi"
+        FRIDAY = "Ven.", "Vendredi"
+        SATURDAY = "Sam.", "Samedi"
+        SUNDAY = "Dim.", "Dimanche"
 
+    day = models.CharField(
+        "jour de la semaine",
+        max_length=10,
+        choices=Day.choices,
+    )
+    opening_hours = models.CharField("horaires d'ouverture", max_length=80)
     garage = models.ForeignKey(
         Garage,
         on_delete=models.CASCADE,
-        default='Garage.objects.first()',
+        default="Garage.objects.first()",
+        verbose_name="garage",
     )
-    day = models.CharField(
-        "jour de la semaine", 
-        max_length=10,
-        choices=Day.choices,
-        )
-    opening_hours = models.CharField("horaires d'ouverture", max_length=80)
 
     def __str__(self) -> str:
         return self.day
-    
+
     class Meta:
-        verbose_name = "Horaire"
+        verbose_name = "horaire"
+
 
 class ServiceType(models.Model):
-    garage = models.ForeignKey(
-        Garage,
-        on_delete=models.CASCADE,
-        default='Garage.objects.first()',
-    )
     name = models.CharField("nom", max_length=50)
-    image = models.ImageField("image", upload_to='uploads/%Y/%m/')
-    
+    image = models.ImageField("image", upload_to="uploads/%Y/%m/")
+
     def __str__(self) -> str:
         return self.name
-    
+
     class Meta:
-        verbose_name = "Types de service"
+        verbose_name = "type de service"
+        verbose_name_plural = "types de service"
+
 
 class Service(models.Model):
+    name = models.CharField("nom", max_length=80)
     type = models.ForeignKey(
         ServiceType,
         on_delete=models.CASCADE,
-        related_name='service_name'
+        related_name="service_name"
     )
-    name = models.CharField("nom", max_length=80)
-
-    def __str__(self) -> str:
-        return self.name
-    
-    class Meta:
-        verbose_name = "Service"
-
-class Vehicle(models.Model):
     garage = models.ForeignKey(
         Garage,
         on_delete=models.CASCADE,
-        default='Garage.objects.first()',
+        default="Garage.objects.first()",
     )
-    name = models.CharField("nom", max_length=80)
-    price = models.IntegerField("prix")
-    year = models.SmallIntegerField("année")
-    km = models.IntegerField("kilométrage")
 
     def __str__(self) -> str:
         return self.name
-    
+
     class Meta:
-        verbose_name = "Véhicule"
+        verbose_name = "service"
+
+
+class Vehicle(models.Model):
+    name = models.CharField("nom", max_length=80)
+    price = models.PositiveIntegerField("prix")
+    year = models.PositiveSmallIntegerField("année")
+    km = models.PositiveIntegerField("kilométrage")
+    garage = models.ForeignKey(
+        Garage,
+        on_delete=models.CASCADE,
+        default="Garage.objects.first()",
+    )
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name = "véhicule"
+
 
 class VehiclePicture(models.Model):
     vehicle = models.ForeignKey(
-        Vehicle, 
-        on_delete=models.CASCADE, 
-        related_name='pictures',
+        Vehicle,
+        on_delete=models.CASCADE,
+        related_name="pictures",
     )
-    picture = models.ImageField("photos", upload_to='uploads/%Y/%m/')
+    picture = models.ImageField("photos", upload_to="uploads/%Y/%m/")
 
     def __str__(self) -> str:
         return self.picture.name
-    
+
     class Meta:
-        verbose_name = "Photos des véhicule"
+        verbose_name = "photos des véhicules"
+        verbose_name_plural = "photos des véhicules"
+
 
 class CustomerReview(models.Model):
-    name = models.CharField("nom", max_length=20)
+    name = models.CharField("nom", max_length=50)
     message = models.TextField("message", max_length=2000)
-    rating = models.SmallIntegerField(
-        "note", 
+    rating = models.PositiveSmallIntegerField(
+        "note",
         validators=[
-            validators.MaxValueValidator(5), 
-            validators.MinValueValidator(1),
+            validators.MaxValueValidator(5, message="La note maximale ne peut dépasser 5 étoiles."),
+            validators.MinValueValidator(1, message="La note minimale ne peut dépasser 1 étoile."),
         ]
     )
-    verified = models.BooleanField("vérifié", default=False)
     valid = models.BooleanField("valide", null=True)
     date = models.DateTimeField("date de réception", auto_now_add=True)
-    # verification_date = models.DateTimeField("date de validation", auto_now=True)
-    # validator = models.CharField("modérateur", max_length=50, null=True)
+
+    # verification_date = models.DateTimeField("date de modération", auto_now=True)
+    # validator = models.CharField("modérateur", max_length=150, null=True)
 
     def __str__(self) -> str:
-        localDate = timezone.localtime(self.date)
+        local_date = timezone.localtime(self.date)
         return (
             f"Témoignage de {self.name} reçu le "
-            f"{localDate.strftime('%d/%m/%Y, %H:%M:%S')}"
+            f"{local_date.strftime('%d/%m/%Y, %H:%M:%S')}"
         )
-    
+
     def list_to_display_stars(self) -> list:
-        list = []
+        list_stars = []
         for i in range(5):
             if self.rating > i:
-                list.append("star")
+                list_stars.append("star")
             else:
-                list.append("empty")
-        return list
-    
+                list_stars.append("empty")
+        return list_stars
+
     class Meta:
-        verbose_name = "Témoignages client"
+        verbose_name = "témoignage client"
+        verbose_name_plural = "témoignages client"
+
 
 class CustomerMessage(models.Model):
-    firstname = models.CharField("prénom", max_length=80)
-    lastname = models.CharField("nom", max_length=80)
-    email = models.EmailField("email", max_length=80)
+    first_name = models.CharField("prénom", max_length=80)
+    last_name = models.CharField("nom", max_length=80)
+    email = models.EmailField(
+        "email",
+        max_length=254,
+        validators=[
+            validators.EmailValidator(message="Entrez une adresse email valide.")
+        ]
+    )
     phone_number = models.CharField(
         "numéro de téléphone",
-        max_length=30,
+        max_length=20,
         validators=[
-            validators.RegexValidator(regex=r'^0[1-9](\d{8})$', message="Saisissez un numéro de téléphone valide, ex: 0123456789.")
+            validators.RegexValidator(
+                regex=r'^0[1-9](\d{8})$',
+                message="Entrez un numéro de téléphone français valide, ex: 0123456789.")
         ]
     )
     subject = models.CharField("sujet", max_length=255, blank=True, null=True)
@@ -155,11 +189,12 @@ class CustomerMessage(models.Model):
     date = models.DateTimeField("date de réception", auto_now_add=True)
 
     def __str__(self) -> str:
-        localDate = timezone.localtime(self.date)
+        local_date = timezone.localtime(self.date)
         return (
-            f"Message de {self.firstname} {self.lastname} reçu le "
-            f"{localDate.strftime('%d/%m/%Y, %H:%M:%S')}"
+            f"Message de {self.first_name} {self.last_name} reçu le "
+            f"{local_date.strftime('%d/%m/%Y, %H:%M:%S')}"
         )
-    
+
     class Meta:
-        verbose_name = "Messages client"
+        verbose_name = "message client"
+        verbose_name_plural = "messages client"
