ea695a92fa1d6323a036c3b94bb42d97f29ff4fb
Floretcl <38957472+floretcl@users.noreply.github.com>
2023-10-14 23:03:07 +0200

    add messages-modal.js

---
+++ garage_vparrot/staticfiles/garage/javascript/messages-modal.js
@@ -0,0 +1,8 @@
+//--------------------- MODAL : FORM SUCCESS MESSAGES  ---------------------------//
+
+const modalMessages = document.querySelector('#modal-messages');
+const modalMessagesCloseBtn = document.querySelector('#modal-messages-close-btn');
+
+modalMessagesCloseBtn.addEventListener('click', () => {
+  modalMessages.classList.add('hidden');
+});
