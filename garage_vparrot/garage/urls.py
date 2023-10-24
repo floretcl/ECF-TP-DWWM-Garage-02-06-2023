539edeb7e4877901fc0b85e1bc7d59b2426a1888
Floretcl <38957472+floretcl@users.noreply.github.com>
2023-10-24 20:29:23 +0200

    Update url of vehicles json data Remove url of vehicles pics json data

--- garage_vparrot/garage/urls.py
+++ garage_vparrot/garage/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
 
-from . import views
+from. import views
 
 app_name = "garage"
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('vehicles/', views.VehiclesView.as_view(), name='vehicles'),
-    path('vehicles-list/', views.VehicleListJsonResponse.as_view(), name='vehicles-json'),
-    path('vehicles-pics/', views.VehiclePicturesJsonResponse.as_view(), name='vehicles-pics-json'),
+    path('vehicles-json/', views.VehiclesJsonResponse.as_view(), name='vehicles-json'),
     path('contact/', views.ContactView.as_view(), name='contact'),
     path('legal-notice/', views.LegalNoticeView.as_view(), name='legal-notice'),
     path('privacy-policy/', views.PrivacyPolicyView.as_view(), name='privacy-policy'),
