80eda04633b054dac1d2fdcb2b34fd397e227648
Floretcl <38957472+floretcl@users.noreply.github.com>
2023-10-24 20:25:05 +0200

    Add vehicles pictures to jsonresponse in VehicleJsonResponse view Remove VehiclePicturesJsonResponse view Simplify some views

--- garage_vparrot/garage/views.py
+++ garage_vparrot/garage/views.py
@@ -1,27 +1,16 @@
 from django.core import serializers
 from django.http import JsonResponse
-from django.views.generic.list import MultipleObjectMixin
 from django.contrib import messages
+from django.forms.models import model_to_dict
+from django.views.generic import (View, TemplateView, FormView, ListView)
 from .modelforms import ContactForm, VehicleContactForm, ReviewForm
-from django.views.generic import (
-    View,
-    TemplateView,
-    FormView,
-    ListView,
-)
-from .models import (
-    OpeningTime,
-    Service,
-    Vehicle,
-    VehiclePicture,
-    CustomerReview,
-)
+from .models import (OpeningTime, Service, Vehicle, VehiclePicture, CustomerReview)
 
 
 class IndexView(View):
     @staticmethod
     def get(request, *args, **kwargs):
-        view = IndexListsView.as_view()
+        view = IndexTemplateView.as_view()
         return view(request, *args, **kwargs)
 
     @staticmethod
@@ -30,28 +19,21 @@ class IndexView(View):
         return view(request, *args, **kwargs)
 
 
-class IndexListsView(ListView):
+class IndexTemplateView(TemplateView):
     template_name = 'garage/index.html'
-    model = CustomerReview
-    context_object_name = 'reviews'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        opening_time = OpeningTime.objects.all()
-        services = Service.objects.all()
-        context["opening_time"] = opening_time
-        context["services"] = services
+        context["services"] = Service.objects.all()
+        context["reviews"] = CustomerReview.objects.filter(valid=True).order_by("-date")[:3]
+        context["opening_time"] = OpeningTime.objects.all()
         context["form"] = ReviewForm()
         return context
 
-    def get_queryset(self):
-        return CustomerReview.objects.filter(valid=True).order_by("-date")[:3]
 
-
-class IndexReviewFormView(MultipleObjectMixin, FormView):
+class IndexReviewFormView(FormView):
     template_name = 'garage/index.html'
     form_class = ReviewForm
-    model = CustomerReview
     success_url = '/'
 
     def form_valid(self, form):
@@ -79,8 +61,7 @@ class VehiclesListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        opening_time = OpeningTime.objects.all()
-        context["opening_time"] = opening_time
+        context["opening_time"] = OpeningTime.objects.all()
         context["vehicles_pictures"] = VehiclePicture.objects.all()
         context["form"] = VehicleContactForm()
         return context
@@ -103,30 +84,17 @@ class VehicleContactFormView(FormView):
         return super().form_valid(form)
 
 
-class VehicleListJsonResponse(ListView):
-    model = Vehicle
-    context_object_name = 'vehicle-list'
+class VehiclesJsonResponse(View):
 
-    def get(self, request, *args, **kwargs):
-        vehicle_list = self.get_queryset()
-        data = serializers.serialize('json', vehicle_list)
-        return JsonResponse(data, safe=False)
-
-    def get_queryset(self):
-        return Vehicle.objects.order_by('id')
-
-
-class VehiclePicturesJsonResponse(ListView):
-    model = VehiclePicture
-    context_object_name = 'vehicle-picture-list'
-
-    def get(self, request, *args, **kwargs):
-        vehicle_picture_list = self.get_queryset()
-        data = serializers.serialize('json', vehicle_picture_list)
-        return JsonResponse(data, safe=False)
-
-    def get_queryset(self):
-        return VehiclePicture.objects.order_by('vehicle')
+    @staticmethod
+    def get(*args, **kwargs):
+        vehicles = Vehicle.objects.order_by('id')
+        vehicle_dict = {}
+        for vehicle in vehicles:
+            vehicle_dict[vehicle.id] = model_to_dict(vehicle)
+            pictures = VehiclePicture.objects.filter(vehicle=vehicle)
+            vehicle_dict[vehicle.id]['pictures'] = [picture.picture.url for picture in pictures]
+        return JsonResponse(vehicle_dict)
 
 
 class ContactView(FormView):
