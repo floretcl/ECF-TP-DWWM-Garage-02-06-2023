67f55391feb0fc538f68543781bf701ab4c69ef3
Floretcl <38957472+floretcl@users.noreply.github.com>
2023-11-08 17:22:19 +0100

    add vehicle pictures placeholder for vehicle detail view, update vehicle detail

--- garage_vparrot/garage/static/garage/javascript/modal-messages.js
+++ garage_vparrot/garage/static/garage/javascript/modal-messages.js
@@ -4,5 +4,5 @@ const modalMessages = document.querySelector('#modal-messages');
 const modalMessagesCloseBtn = document.querySelector('#modal-messages-close-btn');
 
 modalMessagesCloseBtn.addEventListener('click', () => {
-    modalMessages.classList.add('hidden');
+    modalMessages.classList.toggle('hidden');
 });
--- garage_vparrot/garage/static/garage/javascript/modal-vehicle-contact.js
+++ garage_vparrot/garage/static/garage/javascript/modal-vehicle-contact.js
@@ -1,9 +1,21 @@
 //--------------------- MODAL : VEHICLES CONTACT FORM  ---------------------------//
 
-const modalVehicles = document.querySelector('#modal-vehicles');
-const modalVehiclesCloseBtn = document.querySelector('#modal-vehicles-close-btn');
+const modalVehicle = document.querySelector('#modal-vehicles');
+const modalVehicleOpenBtn = document.querySelector('#modal-vehicles-open-btn')
+const modalVehicleCloseBtn = document.querySelector('#modal-vehicles-close-btn');
+const modalVehicleSubject = document.querySelector("#id_subject");
+const vehicleName = document.querySelector('#vehicle-title').getAttribute('title');
 
-modalVehiclesCloseBtn.addEventListener('click', () => {
-    modalVehicles.classList.add('hidden');
-    modalFormInputSubject.value = "";
+
+const toggleModalVehicle = () => {
+    modalVehicle.classList.toggle('hidden');
+}
+
+modalVehicleOpenBtn.addEventListener('click', () => {
+   toggleModalVehicle();
+   modalVehicleSubject.value = vehicleName;
+});
+modalVehicleCloseBtn.addEventListener('click', () => {
+    toggleModalVehicle();
+    modalVehicleSubject.value = "";
 });
